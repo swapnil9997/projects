7cb786707f2f07284d38cec91a353d42be6dd91b
swapnil9997 <134470994+swapnil9997@users.noreply.github.com>
2023-05-24 18:38:44 +0530

    Athena configuration server applications
    application of configuration server

---
+++ athenaconfigrepo/athenaconfigserver/src/main/java/com/athena/configserver/AthenaconfigserverApplication.java
@@ -0,0 +1,15 @@
+package com.athena.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class AthenaconfigserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AthenaconfigserverApplication.class, args);
+	}
+
+}
